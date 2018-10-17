91b5b1d42f2ebdf364af6e2e88a2a75264a04eda
Nakim <public.velopert@gmail.com>
2018-10-17 14:04:49 +0900

    Force white background in jpeg

--- src/handler.ts
+++ src/handler.ts
@@ -95,6 +95,8 @@ export const resize: Handler = async (event: APIGatewayEvent, context: Context, 
     };
 
     const resized = await sharp(buffer)
+      .flatten()
+      .embed()
       .background({ r: 255, g: 255, b: 255, alpha: 1 })
       .resize(nextSize.width, nextSize.height)
       .toFormat('jpeg')
